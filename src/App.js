c5e3afbe2edac8945b0775bd5b32eef4bdb554cc
Marie-Carrere <marie.carrere@icloud.com>
2019-06-04 11:21:03 +0200

    :sparkles: add user data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import UserTable from './tables/UserTables'
 
 const App = () => {
+  const usersData = [
+    { id: 1, name: 'Tania', username: 'Tania Doe' },
+    { id: 2, name: 'Craig', username: 'Craig Doe' },
+    { id: 3, name: 'Ben', username: 'Ben Doe' }
+  ]
+
+  const [users, setUsers] = useState(usersData)
+
   return(
     <div className="container">
       <h1>CRUD App with Hooks</h1>
@@ -10,6 +19,7 @@ const App = () => {
         </div>
         <div className="flex-large">
          <h2>View users</h2>
+         <UserTable users={ users }/>
         </div>
       </div>
     </div>
