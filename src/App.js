43b58a474cb0e972d25ebc190cc8e8e333d3ec50
Marie-Carrere <marie.carrere@icloud.com>
2019-06-04 16:02:10 +0200

    :sparkles: added users form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import UserTable from './tables/UserTables'
+import AddUserForm from './forms/AddUserForm'
 
 const App = () => {
   const usersData = [
@@ -9,6 +10,11 @@ const App = () => {
   ]
 
   const [users, setUsers] = useState(usersData)
+ 
+  const addUser = user => {
+    user.id = users.length +1
+    setUsers([...users, user])
+  }
 
   return(
     <div className="container">
@@ -16,6 +22,7 @@ const App = () => {
       <div className="flex-row">
         <div className="flex-large">
           <h2>Add user</h2>
+          <AddUserForm addUser={ addUser }/>
         </div>
         <div className="flex-large">
          <h2>View users</h2>
