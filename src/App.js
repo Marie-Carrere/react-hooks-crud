b1052ebde3f7e84d8204d734cd2bc7db014a576f
Marie-Carrere <marie.carrere@icloud.com>
2019-06-04 16:09:29 +0200

    :sparkles: Delete users

--- src/App.js
+++ src/App.js
@@ -16,6 +16,10 @@ const App = () => {
     setUsers([...users, user])
   }
 
+  const deleteUser = id => {
+    setUsers(users.filter(user => user.id !== id))
+  }
+
   return(
     <div className="container">
       <h1>CRUD App with Hooks</h1>
@@ -26,7 +30,7 @@ const App = () => {
         </div>
         <div className="flex-large">
          <h2>View users</h2>
-         <UserTable users={ users }/>
+         <UserTable users={ users } deleteUser={ deleteUser }/>
         </div>
       </div>
     </div>
